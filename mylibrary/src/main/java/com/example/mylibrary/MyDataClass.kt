616145f5ea9f9b29a96acdd11ef87c8b5dede30b
Paul Woitaschek <woitaschek@posteo.de>
2018-02-09 14:39:05 +0100

    Initial commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/MyDataClass.kt
@@ -0,0 +1,6 @@
+package com.example.mylibrary
+
+import se.ansman.kotshi.JsonSerializable
+
+@JsonSerializable
+data class MyDataClass(val value: Int)
---
+++ mylibrary/src/main/java/com/example/mylibrary/MyFactory.kt
@@ -0,0 +1,6 @@
+package com.example.mylibrary
+
+import se.ansman.kotshi.KotshiJsonAdapterFactory
+
+@KotshiJsonAdapterFactory
+object MyFactory : KotshiMyFactory()
